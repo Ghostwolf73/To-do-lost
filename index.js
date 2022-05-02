4ccce4c9cd665fd1014de1f4c95465486818eb43
Ghostwolf73 <solomzigumbo@gmail.com>
2022-05-02 10:14:47 +0200

    Can now print day of the week

--- index.js
+++ index.js
@@ -3,14 +3,43 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');  
+
 app.get('/', function(req, res){
     var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
 
-    if(today.getDay() === 6 || today.getDate() === 0){
-        res.send("Weekend");
-    }else {
-        res.send('no weekend');
+    switch(currentDay){
+        case 0:
+            day = 'Sunday';
+            break;
+
+        case 1:
+            day = 'Monday';
+            break;
+        
+        case 2:
+            day = 'Tuesday';
+            break;
+
+        case 3:
+            day = 'Wednesday';
+            break;
+
+        case 4:
+            day = 'Thursday';
+            break;
+
+        case 5:
+            day = 'Friday';
+            break;
+
+        case 6:
+            day = 'Saturday';
+            break;
     }
+    res.render("list", {kindOfDay: day});
 });
 
 app.listen(3000, function(){
