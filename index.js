7644f9972cac495ae99e6a71316a752c39f64b9f
Ghostwolf73 <solomzigumbo@gmail.com>
2022-05-03 07:53:35 +0200

    styled the app

--- index.js
+++ index.js
@@ -3,45 +3,40 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+var tasks = ["Buy food", "Cook food", "Eat"];
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 app.set('view engine', 'ejs');  
 
 app.get('/', function(req, res){
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+    };
 
-    switch(currentDay){
-        case 0:
-            day = 'Sunday';
-            break;
-
-        case 1:
-            day = 'Monday';
-            break;
-        
-        case 2:
-            day = 'Tuesday';
-            break;
-
-        case 3:
-            day = 'Wednesday';
-            break;
-
-        case 4:
-            day = 'Thursday';
-            break;
-
-        case 5:
-            day = 'Friday';
-            break;
-
-        case 6:
-            day = 'Saturday';
-            break;
-    }
-    res.render("list", {kindOfDay: day});
+    var day = today.toLocaleDateString('en-US', options);
+    
+    res.render("list", {
+        kindOfDay: day,
+        newItems: tasks
+    });
 });
+
+app.post('/', function(req, res){
+    var task = req.body.nlist;
+
+    tasks.push(task);
+
+    res.redirect("/");
+})
 
 app.listen(3000, function(){
     console.log("Your server has started on port 3000");
 });
+
+
+
